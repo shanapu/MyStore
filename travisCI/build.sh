0a2f56ddeacb45a40e95f17a195e5019cc15a921
shanapu <thomas@shanapu.de>
2019-08-04 04:27:16 +0200

    will it end here?

--- travisCI/build.sh
+++ travisCI/build.sh
@@ -58,6 +58,7 @@ mkdir build/gameserver
 mkdir build/fastDL
 
 echo "Move addons, materials and sound folder"
+mv addons build/gameserver
 echo mv addons cfg materials models sound particles build/gameserver
 
 echo "Move FastDL folder"
